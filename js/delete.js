e5ce3b5bf1aa07eb3f075bdec24447cceb5f0ef6
jkillz2020 <kilgore.josh@gmail.com>
2016-09-10 13:48:58 -0500

    add delete functionality remove message object from array

---
+++ js/delete.js
@@ -0,0 +1,15 @@
+var Chatty = (function(deleteMessage){
+
+
+  //  remove message
+   deleteMessage.removeMessage = function(id) {
+    var messages = Chatty.getDefaultMessages();
+      var selectedIndex = messages.findIndex(function(message){
+        return message.id === id;
+      });
+      messages.splice(selectedIndex, 1);
+      
+   };
+
+return deleteMessage;
+})(Chatty  || {});
