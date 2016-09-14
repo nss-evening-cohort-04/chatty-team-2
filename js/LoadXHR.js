711589d03600e2fdd5c3f12e10248b170e993af2
justin leggett <justin@codingrookie.com>
2016-09-13 20:00:01 -0500

    moved load fimction in loadXhr to loadCurrentMessages function

--- js/LoadXHR.js
+++ js/LoadXHR.js
@@ -4,18 +4,11 @@ var Chatty = (function(addDom) {
     addDom.loadXhr = function() {
         var jsonFiles = ["json/message1.json", "json/message2.json", "json/message3.json", "json/message4.json", "json/message5.json"];
         for(var i = 0; i < jsonFiles.length; i++) {
-        var messageRequest = new XMLHttpRequest();
-        messageRequest.addEventListener("load", function() {
-            var data = JSON.parse(this.responseText);
-            for(var i = 0; i < data.messages.length; i++) {
-                currentMessages.push(data.messages[i]);
-            }
-            Chatty.setMessages(currentMessages);
-            Chatty.loadMessages();
-        });
-        messageRequest.addEventListener("error", Chatty.onError)
-        messageRequest.open("GET", jsonFiles[i]);
-        messageRequest.send();
+            var messageRequest = new XMLHttpRequest();
+            messageRequest.addEventListener("load", addDom.loadCurrentMessages);
+            messageRequest.addEventListener("error", Chatty.onError);
+            messageRequest.open("GET", jsonFiles[i]);
+            messageRequest.send();
     }
 }
 
@@ -26,6 +19,16 @@ addDom.getJSONMessages = function() {
 addDom.onError = function() {
     console.log("There was an error processing the file.");
 }
+
+addDom.loadCurrentMessages = function() {
+    var data = JSON.parse(this.responseText);
+        // added for loop incase additinal elements are added to files
+        for(var i = 0; i < data.messages.length; i++) {
+            currentMessages.push(data.messages[i]);
+        }
+    Chatty.setMessages(currentMessages);
+    Chatty.loadMessages();
+};
 
 return addDom;
 })(Chatty || {});
